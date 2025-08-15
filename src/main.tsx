a49c9048a4070b660450aee9d94a66e7e1ee94c8
priyanshu2304 <priyanshumodi1998@gmail.com>
2025-08-15 15:25:09 +0530

    Register service worker on load for improved offline capabilities

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(console.error);
+  });
+}
+
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
@@ -11,9 +17,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(console.error);
-  });
-}
